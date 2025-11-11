0de199446161d7f2d603a3410a28a9b84aaf69da
ohchanju <ohchanju3@naver.com>
2025-11-12 07:37:38 +0900

    [Feat] admin auth 관련 수정 #40

--- packages/core/src/api/auth.ts
+++ packages/core/src/api/auth.ts
@@ -2,13 +2,19 @@ export function setTokens({
   access,
   refresh,
   careGrade,
+  name,
 }: {
   access: string;
   refresh: string;
   careGrade?: string | null;
+  name?: string;
 }) {
   localStorage.setItem("access", access);
   localStorage.setItem("refresh", refresh);
+  if (name) {
+    localStorage.setItem("name", name);
+  }
+
   if (careGrade) {
     localStorage.setItem("careGrade", careGrade);
   }
@@ -18,4 +24,5 @@ export function clearTokens() {
   localStorage.removeItem("access");
   localStorage.removeItem("refresh");
   localStorage.removeItem("careGrade");
+  localStorage.removeItem("name");
 }
