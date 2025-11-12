cb76a66b0a1f4e261faba76d4be664c9a7817902
ohchanju <ohchanju3@naver.com>
2025-11-12 14:39:09 +0900

    [Refact] 프로그램 추가 모달 코드 리팩토링 #45

--- apps/admin/src/pages/AdminProgram.tsx
+++ apps/admin/src/pages/AdminProgram.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import AddProgramModal from "@components/program/AddProgramModal";
+import AddProgramModal from "@components/program/AddProgramModal/AddProgramModal";
 
 const AdminProgram = () => {
   const [open, setOpen] = useState(false);
