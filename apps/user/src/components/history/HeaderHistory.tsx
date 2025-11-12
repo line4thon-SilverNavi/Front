b6188453562f27040c7446786524b827531e8a03
김연비 <yeonbee@gim-yeonbiui-MacBookPro.local>
2025-11-12 23:59:53 +0900

    [feat]내역 헤더 구현

---
+++ apps/user/src/components/history/HeaderHistory.tsx
@@ -0,0 +1,51 @@
+import InputContainer from "@core/components/inputContainer";
+import { useState } from "react";
+import styled from "styled-components";
+
+
+export default function HeaderHistory(){
+    const [keyword, setKeyword] = useState("");
+    const [noti, setNoti] = useState(false);
+
+    return(
+        <Wrapper>
+            <SearchContainer>
+            <InputContainer
+                label=""
+                placeholder="내용을 입력하여 검색하세요."
+                value={keyword}
+                onChange={(value) => setKeyword(value)}
+                variant="filled"
+                inputTypo="body2"
+                width="100%"
+            />
+            <SearchIcon src="/img/header/search.png" alt="search" />
+            </SearchContainer>
+            <img src={noti ? "/img/header/notification-red.png" : "/img/header/notification.png"} 
+            style={{height:"26px", width:"25px", marginTop:"5px"}} onClick={() => setNoti(false)}/>
+        </Wrapper>
+    );
+}
+
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+`;
+
+const SearchContainer = styled.div`
+    position: relative;
+    width: 90%;
+`;
+
+const SearchIcon = styled.img`
+    position: absolute;
+    right: 16px;
+    top: 57%;
+    transform: translateY(-50%);
+    width: 24px;
+    height: 24px;
+    z-index: 10;
+    pointer-events: none;
+`;
--- apps/user/src/pages/history/History.tsx
+++ apps/user/src/pages/history/History.tsx
@@ -1,6 +1,13 @@
+import DefaultLayout from "@layouts/DefaultLayout";
+import HeaderHistory from "@components/history/HeaderHistory";
+import NavBar from "@components/common/NavBar";
+
 export default function History(){
     return(
-        <p>내역 페이지</p>
+        <DefaultLayout header={<HeaderHistory />} footer={<NavBar />}>
+
+
+        </DefaultLayout>
     );
 }
 
