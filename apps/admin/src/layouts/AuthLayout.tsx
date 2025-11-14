490f632a79a34fa9203e7f8581d9e3a72391d856
김연비 <yeonbee@gim-yeonbiui-MacBookPro.local>
2025-11-14 23:07:21 +0900

    [fix]auth 수정

--- apps/admin/src/layouts/AuthLayout.tsx
+++ apps/admin/src/layouts/AuthLayout.tsx
@@ -29,6 +29,7 @@ const AuthLayoutWrapper = styled.div`
   padding-bottom: calc(
     clamp(16px, 5vh, 25px) + env(safe-area-inset-bottom, 0px)
   );
+  background-color: white;
 `;
 const TitleArea = styled.div`
   width: 100%;
