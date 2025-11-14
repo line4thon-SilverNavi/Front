315e8963701719bebd0e748d22d0f4b61d20b2ac
ohchanju <ohchanju3@naver.com>
2025-11-14 15:42:43 +0900

    [Feat] review list #77

---
+++ apps/admin/src/components/common/pagination.tsx
@@ -0,0 +1,5 @@
+// const pagination =() => {
+//   return (
+//     <
+//   )
+// }
--- apps/admin/src/pages/Review.tsx
+++ apps/admin/src/pages/Review.tsx
@@ -1,28 +1,41 @@
+// src/pages/review/Review.tsx
 import { useEffect, useState } from "react";
 import ReviewStatusCard from "@components/review/ReviewCard";
-import { getReviews, type ReviewSummary } from "@apis/review/getReviews";
+import ReviewList from "@components/review/ReviewList";
+import {
+  getReviews,
+  type ReviewSummary,
+  type ReviewItem,
+} from "@apis/review/getReviews";
 import toast from "react-hot-toast";
 
 const Review = () => {
   const [summary, setSummary] = useState<ReviewSummary | null>(null);
+  const [reviews, setReviews] = useState<ReviewItem[]>([]);
 
   useEffect(() => {
-    async function fetchSummary() {
+    async function fetchInitial() {
       try {
-        const res = await getReviews();
+        const res = await getReviews(); // 첫 페이지, 전체
         setSummary(res.summary);
+        setReviews(res.reviews);
       } catch (err) {
         console.error(err);
         toast.error("리뷰 정보를 불러오지 못했습니다.");
       }
     }
 
-    fetchSummary();
+    fetchInitial();
   }, []);
 
-  if (!summary) return <div>불러오는 중...</div>;
+  if (!summary) return <div>불러오는 중</div>;
 
-  return <ReviewStatusCard summary={summary} />;
+  return (
+    <>
+      <ReviewStatusCard summary={summary} />
+      <ReviewList items={reviews} />
+    </>
+  );
 };
 
 export default Review;
